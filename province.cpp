89d854af9accf1a515864a50265907af078f898b
mafazely <aminfazeli00@gmail.com>
2020-11-22 00:25:40 +0330

    add some part of forking section

--- province.cpp
+++ province.cpp
@@ -62,10 +62,71 @@ int main(int argc, char const *argv[])
 
     /* ---------------------------------------------------------------------- */
 
-    /* handling fork and city childs */
+    /* -------------- handling fork and city childs --------------- */
 
-    /***************************/
+    std::vector<std::vector<int>> cityPipes;
+    std::vector<pid_t> citypids;
+    std::vector<std::string> citynamedpipes;
+    std::string execPath;
+    std::string cityfifo;
 
+    /* create unnamed pipes */
+    for (int i = 0; i < cityCount; i++)
+    {
+        int cityfd[2];
+        if (pipe(cityfd) < 0)
+        {
+            std::cerr << "can not create city pipe " << i << std::endl;
+            return 2;
+        }
+        std::vector<int> fds;
+        fds.push_back(cityfd[0]);
+        fds.push_back(cityfd[1]);
+        cityPipes.push_back(fds);
+    }
+    /* -------------------- */
+
+
+    /* create fifos */
+    for (int i = 0; i < cityCount; i++)
+    {
+        cityfifo = std::string(PROVINCENAMEDPIPE) + std::to_string(i);
+        mkfifo(cityfifo.c_str(), 0666);
+        citynamedpipes.push_back(cityfifo);
+    }
+    /* ------------ */
+
+    
+    /*--- forking --- */
+    for (int i = 0; i < cityCount; i++)
+    {
+        execPath = ProvinceFilePath + '/' + cities[i];
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            std::cerr << "can not fork" << std::endl;
+        }
+        else if (pid == 0)
+        {
+            close(cityPipes[i][1]);
+            const char *argv[5];
+            argv[0] = "city";
+            argv[1] = execPath.c_str();
+            argv[2] = std::to_string(cityPipes[i][0]).c_str();
+            argv[4] = citynamedpipes[i].c_str();
+            argv[5] = NULL;
+
+            execv(argv[0], (char **)argv);
+        }
+        else
+        {
+            close(cityPipes[i][0]);
+            citypids.push_back(pid);
+        }
+    }
+    /*-------------- */
+
+    /***************************************************************/
 
     /* getting data from cities */
 
@@ -89,6 +150,6 @@ int main(int argc, char const *argv[])
         std::cerr << "error in closing namedpipe [FIFO]" << std::endl;
         return 2;
     }
-    
+
     /* ------------------------------------------- */
 }
