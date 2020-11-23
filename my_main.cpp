a201e7e27d721ab8b44e5b0fc54369cd0dc22872
mafazely <aminfazeli00@gmail.com>
2020-11-23 19:00:51 +0330

    fix main file

--- my_main.cpp
+++ my_main.cpp
@@ -1,43 +1,13 @@
-//#include <iostream>
-//#include <vector>
-//#include <map>
-#include <dirent.h>
-//#include <unistd.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-//#include <fstream>
-//#include <string.h>
-#include <bits/stdc++.h>
+#include "my_loadBalancer.hpp"
 
 int main(int argc, char const *argv[])
 {
-    std::string dirPath;
-    std::vector<std::string> dirFiles;
-
-    DIR *dir;
-    struct dirent *ent;
-
-    dirPath = "./" ;
-
-    if ((dir = opendir(dirPath.c_str())) != NULL)
-    {
-        while ((ent = readdir(dir)) != NULL)
-        {
-            if (strcmp(ent->d_name, ".") && strcmp(ent->d_name, ".."))
-            {
-                dirFiles.push_back(ent->d_name);
-            }
-        }
-        closedir(dir);
-    }
-    else
-    { /* could not open directory */
-        perror("open directory failed!");
-        exit(1);
-    }
-    sort(dirFiles.begin(), dirFiles.end());
-    for (int i = 0; i < dirFiles.size() ; i++)
-        std::cout << dirFiles[i] << std::endl;
-
-        return 0;
+    LoadBalancer LoadBalancer(argv[1], argv[2]);
+    LoadBalancer.InputHandler();
+    LoadBalancer.CreateNamedPipe();
+    LoadBalancer.CreateDistributorPipes();
+    LoadBalancer.CreateDistributors();
+    LoadBalancer.ReceiveDataFromDistributors();
+    LoadBalancer.ShowFinalResult();
+    //sleep(20);
 }
