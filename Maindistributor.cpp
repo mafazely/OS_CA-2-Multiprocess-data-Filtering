f8b7f9d805c6cdbcd480015ec26f46e9bfff7556
mafazely <aminfazeli00@gmail.com>
2020-11-23 19:00:08 +0330

    fix distributor 's fucking bugs

--- Maindistributor.cpp
+++ Maindistributor.cpp
@@ -9,14 +9,15 @@
 
 #include "my_defines.hpp"
 #include "Tools.hpp"
+//#define DIRPATHPASSETS "./main_CA/OS-CA2-Sample/assets"
 
 int main(int argc, char const *argv[])
 {
     std::string AssetFilePath;
-
+    std::string my_msg = "2018/01/01@2020/01/01@0@MAX";
     std::string AssetfifoPath;
     std::string Asset_input;
-    std::string Asset_final_res;
+    std::string Assetfinal_res;
     std::string province_final_res;
     std::string Asset_order;
 
@@ -32,8 +33,9 @@ int main(int argc, char const *argv[])
     char Asset_buff[MSGSIZE];
     char province_Buff[MSGSIZE];
 
+    //AssetFilePath = DIRPATHPASSETS;
     AssetFilePath = std::string(argv[1]);    // path of Assets directory that should searching for own provinces
-    Asset_fd = std::stoi(argv[2]);           // file descriptor for reading from unnamed pipe
+    Asset_fd = atoi(argv[2]);                // file descriptor for reading from unnamed pipe
     AssetfifoPath = std::string(argv[3]);    // path of named pipe for sending filtered result
 
     provinces = getDirFiles(AssetFilePath); // get list of directories in this dir
@@ -65,7 +67,7 @@ int main(int argc, char const *argv[])
 
     std::vector<std::vector<int>> AssetPipes;
     std::vector<std::string> Assetnamedpipes;
-    std::string AssetexecPath;
+    std::string AssetExecPath;
     std::string Assetfifo;
 
     /* create unnamed pipes */
@@ -77,26 +79,28 @@ int main(int argc, char const *argv[])
             std::cerr << "can not create province pipe " << i << std::endl;
             return 2;
         }
-        std::vector<int> fds;
-        fds.push_back(Assetfd[0]);
-        fds.push_back(Assetfd[1]);
-        AssetPipes.push_back(fds);
+        std::vector<int> distfds;
+        distfds.push_back(Assetfd[0]);
+        distfds.push_back(Assetfd[1]);
+        AssetPipes.push_back(distfds);
     }
     /* -------------------- */
 
     /* create fifos */
     for (int i = 0; i < ProvinceCount; i++)
     {
-        Assetfifo = std::string(DISTRIBUTORNAMEDPIPE) + std::to_string(i);
+        Assetfifo = std::string(DISTRIBUTORNAMEDPIPE) + provinces[i] ;
         mkfifo(Assetfifo.c_str(), 0666);
+        //std::cout << "fifo name :" << Assetfifo << std::endl;
         Assetnamedpipes.push_back(Assetfifo);
     }
     /* ------------ */
-
+    //printf("before forking\n");
     /*--- forking --- */
     for (int i = 0; i < ProvinceCount; i++)
     {
-        AssetexecPath = AssetFilePath + '/' + provinces[i];
+        AssetExecPath = AssetFilePath + '/' + provinces[i];
+        //std::cout << "province exec path : " << AssetExecPath << std::endl;
         pid_t pid = fork();
         if (pid < 0)
         {
@@ -107,7 +111,7 @@ int main(int argc, char const *argv[])
             close(AssetPipes[i][1]);
             const char *argv[5];
             argv[0] = "province";
-            argv[1] = AssetexecPath.c_str();
+            argv[1] = AssetExecPath.c_str();
             argv[2] = std::to_string(AssetPipes[i][0]).c_str();
             argv[3] = Assetnamedpipes[i].c_str();
             argv[4] = NULL;
@@ -127,32 +131,34 @@ int main(int argc, char const *argv[])
                 std::cerr << "error in closing province pipe" << std::endl;
                 return 2;
             }
+            sleep(1);
         }
     }
     /*-------------- */
     /***************************************************************/
-
+    //std::cout << "before getting data" << std::endl;
     /* getting data from cities */
-
     for (int i = 0; i < ProvinceCount; i++)
     {
-        int fifo_fd;
-        fifo_fd = open(Assetnamedpipes[i].c_str(), O_RDONLY); // open namedpipe to receive filtered data from cities
+        int dist_fifo_fd;
+        dist_fifo_fd = open(Assetnamedpipes[i].c_str(), O_RDONLY); // open namedpipe to receive filtered data from cities
 
-        if (read(fifo_fd, province_Buff, MSGSIZE) < 0)
+        if (read(dist_fifo_fd, province_Buff, MSGSIZE) < 0)
         {
             std::cerr << "error in receiving final res from provinces via FIFO" << std::endl;
             return 2;
         }
-        if (close(fifo_fd) < 0)
+        if (close(dist_fifo_fd) < 0)
         {
             std::cerr << "error in closing namedpipe [FIFO]" << std::endl;
             return 2;
         }
+        unlink(Assetnamedpipes[i].c_str());
 
-        Asset_final_res = std::string(province_Buff);
-        if (Asset_final_res != "-1")
-            Asset_integerResults.push_back(stoi(Asset_final_res));
+        province_final_res = std::string(province_Buff);
+        //std::cout << "province " << i << " final res : " << province_final_res << std::endl;
+        if (province_final_res != "-1" && province_final_res.length() > 0)
+            Asset_integerResults.push_back(stoi(province_final_res));
     }
 
     while (wait(NULL) > 0 || errno != ECHILD);
@@ -161,11 +167,12 @@ int main(int argc, char const *argv[])
     /* ---------------------------------------------------------------------- */
 
     /* calculatios and send final result to parent */
-
-    Asset_final_res = findMinMax(Asset_integerResults, Asset_order);
+    //std::cout << "before find final res " << std::endl;
+    Assetfinal_res = findMinMax(Asset_integerResults, Asset_order);
+    //std::cout << "Final Distributor res is : " << Assetfinal_res << std::endl;
 
     Asset_fifo = open(AssetfifoPath.c_str(), O_WRONLY);         // open namedpipe to send data
-    if (write(Asset_fifo, Asset_final_res.c_str(), (Asset_final_res.length()) + 1) < 0)
+    if (write(Asset_fifo, Assetfinal_res.c_str(), (Assetfinal_res.length()) + 1) < 0)
     {
         std::cerr << "error in sending final Asset result via FIFO" << std::endl;
         return 2;
