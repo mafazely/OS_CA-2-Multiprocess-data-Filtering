e53faae8f332b06617f1b87bbcd2d32354cdf9fd
mafazely <aminfazeli00@gmail.com>
2020-11-23 18:58:20 +0330

    update defines.hpp

---
+++ my_defines.hpp
@@ -0,0 +1,17 @@
+#ifndef MY_DEFINES_HPP
+#define MY_DEFINES_HPP
+
+
+#define MSGSIZE 1024
+#define DELIMITER "@"
+
+#define MIN "MIN"
+#define MAX "MAX"
+
+#define LOADBALANCER "/tmp/loadbalancerfifo"
+#define DISTRIBUTORNAMEDPIPE "/tmp/distfifo"
+#define PROVINCENAMEDPIPE "/tmp/provincefifo"
+#define CITYNAMEDPIPE "/tmp/cityfifo"
+
+
+#endif
